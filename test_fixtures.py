c5749c6d01484df13c800e731abd85b3a4301ba9
SaraNurtazayeva <nurtazaeva.sara@gmail.com>
2023-12-01 00:47:38 +0600

    добавлены доработки по фикстуре

--- test_fixtures.py
+++ test_fixtures.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.fixture
 def browser():
     print("Я выполняюсь перед тестами.")
@@ -7,13 +8,16 @@ def browser():
     yield
     print("Я выполняюсь после теста.")
 
+
 @pytest.fixture
 def login_page(browser):
     pass
 
+
 @pytest.fixture
 def credentials():
     return "admin", "1234"
+
 
 def test_login(login_page, credentials):
     assert credentials == ("admin", "1234"), "Неверный логин или пароль"
--- test_homework2.py
+++ test_homework2.py
@@ -1,13 +1,31 @@
 from selene.support.shared import browser
 from selene import be, have
+import pytest
 
-def test_google_has_no_answer():
+
+@pytest.fixture
+def browser_configs():
+    browser.config.window_width = 500
+    browser.config.window_height = 1920
+    yield
+    browser.quit()
+
+
+@pytest.fixture
+def browser_configs_wider():
+    browser.config.window_width = 2000
+    browser.config.window_height = 1500
+    yield
+    browser.quit()
+
+
+def test_google_has_no_answer(browser_configs):
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('tdxqyg2d27w3fruevhesrht3t3oht5495i9g4').press_enter()
-    browser.element('p[role=heading]').should(have.text('По запросу tdxqyg2d27w3fruevhesrht3t3oht5495i9g4 ничего не найдено.'))
+    browser.element('[name="q"]').should(be.blank).type('tdxqyg2d27w3fruevhesrht3t3oht5').press_enter()
+    browser.element('p[role=heading]').should(have.text('По запросу tdxqyg2d27w3fruevhesrht3t3oht5 ничего не найдено.'))
 
-def test_google_has_answer():
+
+def test_google_has_answer(browser_configs_wider):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
